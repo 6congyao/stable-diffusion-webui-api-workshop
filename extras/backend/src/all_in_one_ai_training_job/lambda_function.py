2ea9110acefa700c74aa1ed1e0b3650a7b675cad
Sharon.T <sharon.librae@gmail.com>
2023-06-05 17:21:47 +0800

    Update lambda_function.py

--- extras/backend/src/all_in_one_ai_training_job/lambda_function.py
+++ extras/backend/src/all_in_one_ai_training_job/lambda_function.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
         body = json.loads(event['body'])
         training_job_name = body['training_job_name']
         hyperparameters = body['hyperparameters']
-        hyperparameters['train-args'] = str(hyperparameters['train-args'])
+        hyperparameters['train-args'] = json.dumps(hyperparameters['train-args'])
         algorithm_specification = body['algorithm_specification']
         role_arn = body['role_arn']
         input_data_config = body['input_data_config']
@@ -20,8 +20,8 @@ def lambda_handler(event, context):
         tags = body['tags']
     
         response = sagemaker_client.create_training_job(
-            TrainingJobName = training_job_name,
-            HyperParameters = hyperparameters,
+            TrainingJobName = str(training_job_name),
+            HyperParameters = json_encode_hyperparameters(hyperparameters),
             AlgorithmSpecification = algorithm_specification,
             RoleArn = role_arn,
             InputDataConfig = input_data_config,
@@ -48,3 +48,9 @@ def lambda_handler(event, context):
             'statusCode': 400,
             'body': str(e)
         }
+    
+def json_encode_hyperparameters(hyperparameters):
+    for (k, v) in hyperparameters.items():
+        print(k, v)
+
+    return {str(k): json.dumps(v) for (k, v) in hyperparameters.items()}
